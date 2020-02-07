86ef06a20eb5c22e447feda9c2a704bcc0f576d7
cfobrien <conor.obrien24@gmail.com>
2020-02-07 17:49:46 +0000

    actual unet lmao

--- monochromatic_test.m
+++ monochromatic_test.m
@@ -31,22 +31,18 @@ num_channels = 32;
 layers = [
     imageInputLayer([input_size input_size 1], 'Name','Input')
     
-    % Depth 1
+    %Depth1
     convolution2dLayer(3,num_channels,'Padding','same','Name','conv11')
     batchNormalizationLayer('Name','bn11')
     reluLayer('Name','relu11')
-
+    
     convolution2dLayer(3,num_channels,'Padding','same','Name','conv12')
     batchNormalizationLayer('Name','bn12')
     reluLayer('Name','relu12')
-
-    convolution2dLayer(3,num_channels,'Padding','same','Name','conv13')
-    batchNormalizationLayer('Name','bn13')
-    reluLayer('Name','relu13')
     
     maxPooling2dLayer(2,'Stride',2,'Name','MP1')
     
-    % Depth 2
+    %Depth2
     convolution2dLayer(3,num_channels*2,'Padding','same','Name','conv21')
     batchNormalizationLayer('Name','bn21')
     reluLayer('Name','relu21')
@@ -55,9 +51,9 @@ layers = [
     batchNormalizationLayer('Name','bn22')
     reluLayer('Name','relu22')
     
-    maxPooling2dLayer(2,'Stride',2, 'Name','MP2')
+    maxPooling2dLayer(2,'Stride',2,'Name','MP2')
     
-    % Depth 3
+    %Depth3
     convolution2dLayer(3,num_channels*4,'Padding','same','Name','conv31')
     batchNormalizationLayer('Name','bn31')
     reluLayer('Name','relu31')
@@ -66,11 +62,61 @@ layers = [
     batchNormalizationLayer('Name','bn32')
     reluLayer('Name','relu32')
     
-    transposedConv2dLayer(2,num_channels*2,'Stride',2,'Name','up32')
+    maxPooling2dLayer(2,'Stride',2,'Name','MP3')
+    
+    %Depth4
+    convolution2dLayer(3,num_channels*8,'Padding','same','Name','conv41')
+    batchNormalizationLayer('Name','bn41')
+    reluLayer('Name','relu41')
+    
+    convolution2dLayer(3,num_channels*8,'Padding','same','Name','conv42')
+    batchNormalizationLayer('Name','bn42')
+    reluLayer('Name','relu42')
+    
+    maxPooling2dLayer(2,'Stride',2,'Name','MP4')
+    
+    %Depth5
+    convolution2dLayer(3,num_channels*16,'Padding','same','Name','conv51')
+    batchNormalizationLayer('Name','bn51')
+    reluLayer('Name','relu51')
+    
+    convolution2dLayer(3,num_channels*16,'Padding','same','Name','conv52')
+    batchNormalizationLayer('Name','bn52')
+    reluLayer('Name','relu52')
+    
+    transposedConv2dLayer(2,num_channels*8,'Stride',2,'Name','up53')
+    batchNormalizationLayer('Name','bn53')
+    reluLayer('Name','relu53')
+    
+    %up-Depth4
+    depthConcatenationLayer(2,'Name','concat4')
+    convolution2dLayer(3,num_channels*8,'Padding','same','Name','conv43')
+    batchNormalizationLayer('Name','bn43')
+    reluLayer('Name','relu43')
+    
+    convolution2dLayer(3,num_channels*8,'Padding','same','Name','con44')
+    batchNormalizationLayer('Name','bn44')
+    reluLayer('Name','relu44')
+    
+    transposedConv2dLayer(2,num_channels*4,'Stride',2,'Name','up45')
+    batchNormalizationLayer('Name','bn45')
+    reluLayer('Name','relu45')
+    
+    %up-Depth3
+    depthConcatenationLayer(2,'Name','concat3')
+    convolution2dLayer(3,num_channels*4,'Padding','same','Name','conv33')
     batchNormalizationLayer('Name','bn33')
     reluLayer('Name','relu33')
-     
-    % Back to depth 2
+    
+    convolution2dLayer(3,num_channels*4,'Padding','same','Name','conv34')
+    batchNormalizationLayer('Name','bn34')
+    reluLayer('Name','relu34')
+    
+    transposedConv2dLayer(2,num_channels*2,'Stride',2,'Name','up35')
+    batchNormalizationLayer('Name','bn35')
+    reluLayer('Name','relu35')
+    
+    %up-Depth2
     depthConcatenationLayer(2,'Name','concat2')
     convolution2dLayer(3,num_channels*2,'Padding','same','Name','conv23')
     batchNormalizationLayer('Name','bn23')
@@ -80,12 +126,16 @@ layers = [
     batchNormalizationLayer('Name','bn24')
     reluLayer('Name','relu24')
     
-    transposedConv2dLayer(2,num_channels*2,'Stride',2,'Name','up25')
+    transposedConv2dLayer(2,num_channels,'Stride',2,'Name','up25')
     batchNormalizationLayer('Name','bn25')
     reluLayer('Name','relu25')
     
-    % Back to depth 1
+    %up-Depth1
     depthConcatenationLayer(2,'Name','concat1')
+    convolution2dLayer(3,num_channels,'Padding','same','Name','conv13')
+    batchNormalizationLayer('Name','bn13')
+    reluLayer('Name','relu13')
+    
     convolution2dLayer(3,num_channels,'Padding','same','Name','conv14')
     batchNormalizationLayer('Name','bn14')
     reluLayer('Name','relu14')
@@ -94,21 +144,20 @@ layers = [
     batchNormalizationLayer('Name','bn15')
     reluLayer('Name','relu15')
     
-    convolution2dLayer(3,1,'Padding','same','Name','conv16')
-    batchNormalizationLayer('Name','bn16')
-    reluLayer('Name','relu16')
-    
     additionLayer(2,'Name','add_end')
     
     regressionLayer('Name','output')
+
 ];
 
 lgraph = layerGraph(layers);
+lgraph = connectLayers(lgraph, 'relu42', 'concat4/in2');
+lgraph = connectLayers(lgraph, 'relu32', 'concat3/in2');
 lgraph = connectLayers(lgraph, 'relu22', 'concat2/in2');
-lgraph = connectLayers(lgraph, 'relu13', 'concat1/in2');
+lgraph = connectLayers(lgraph, 'relu12', 'concat1/in2');
 lgraph = connectLayers(lgraph, 'Input', 'add_end/in2');
 
-%analyzeNetwork(lgraph);
+analyzeNetwork(lgraph);
 
 %% Generate back projected set
 if (GEN_SET)
@@ -190,9 +239,9 @@ end
 
 %% Test net
 if (TEST_NET)
-    gt = fitsread('../datasets/augmented_dataset_linscale/gen_groundtruth_0.fits');
+    gt = fitsread('../datasets/augmented_dataset_linscale/gen_groundtruth_10.fits');
     %bp = fitsread('../back_projections/gen_groundtruth_0.fits');
-    bp = get_bp('../datasets/augmented_dataset_linscale/gen_groundtruth_0.fits');
+    bp = get_bp('../datasets/augmented_dataset_linscale/gen_groundtruth_10.fits');
     load trainednet1;
     res = cell2mat(compute_net(trainednet1, bp, input_size));
     figure
