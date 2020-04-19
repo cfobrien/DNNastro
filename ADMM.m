6ff8db69bd019d25f3cb3aff841bbe87ba3fa991
Andrew Shaw <andrewjshaw98@gmail.com>
2020-04-19 13:54:13 +0100

    working ADMM

--- ADMM.m
+++ ADMM.m
@@ -2,7 +2,6 @@ close all;
 clear;
 clc;
 
-
 %% Setting up paths
 addpath('../matlab_files/');
 addpath('../matlab_files/utils/');
@@ -32,64 +31,49 @@ path = pwd;
 Nx = 512;
 Ny = 512;
 f = 1.4;
-super_res = 1;%super_res=0; 
-sigma = 0.07;%30; ASK MATTHIEU ABOUT VALUE
+super_res = 1;
+sigma = 0.07;
 
 % Noise addition operator
 add_noise = @(y) (y + (randn(size(y)) + 1i*randn(size(y)))*sigma/sqrt(2));
 
-% Iterate over all images in set
-for  i = 1 : 1%numel(filenames)
+for i = 1 : 1 %numel(filenames)
+    
     filename = filenames(i).name;
-    cd('../matlab_files');
 
-    gt = fitsread(filename);
-    max_gt = max(max(gt));
-    
-    gt = imresize((normalise(gt)),[512 512]);
-    
-    clear A; clear At; clear Gw;
-    clear Phi_t;
-    clear Phi;
-    
-    % 2. Create the measurement operator and its adjoint
+    % Create the measurement operator and its adjoint
     [A, At, Gw] = generate_data_basic(Nx,Ny,f,super_res,0);
-    
     Phi_t = @(x) HS_forward_operator(x,Gw,A);
     Phi = @(y) HS_adjoint_operator(y,Gw,At,Nx,Ny);
-
-    
-    
-    % 3. Create the measurements and the back-projection
+       
+    gt = imresize((normalise(fitsread(filename))),[512 512]);
+    max_gt = max(gt(:));
     y = cell2mat(Phi_t(gt));
-    bp = real(Phi({add_noise(y)}));
-    bp = normalise(bp);
+    bp = normalise(real(Phi({add_noise(y)})));
     bp = bp .* max_gt;
     
     [alphad,S]=wavedec2(bp,nlevel,wv);
-    Psit = @(x) wavedec2(x, nlevel,wv); 
+    Psi_t = @(x) wavedec2(x,nlevel,wv); 
     Psi = @(x) waverec2(x,S,wv);
-
-    %setup convergence loop
-    f = @(x) sum(abs(Psit(x)));
+    
+    f = @(x) sum(abs(Psi_t(x)));
     %f = @(x) norm(y - Phit(x), 2)^2;
-
+    
     shrink = @(z, d) max(abs(z)-d, 0).*sign(z);
     vt = zeros(size(y));
     st = -y;
     xt = ones(512);
 
-    
-    %epsilon = sigma * sqrt(M + 2*sqrt(M));
-    epsilon = sigma * sqrt(size(y) + 2 * sqrt(size(y)));
+    M = 0.11*Nx*Ny;
+    epsilon = sigma * sqrt(M + 2*sqrt(M));
     
     sc = @(z) z*min(epsilon/norm(z(:)), 1);
     nt = sc(st);
-    delta = 1e0;
-    rho = 1e2; %threshold value
-    rel_tol = 1e-4;
-    rel_tol2 = 1e-5;
-    max_iter = 100;
+    delta = 1e-7;
+    rho = 1e-7; %threshold value
+    rel_tol = 1e-2;
+    rel_tol2 = 1e-2;
+    max_iter = 1000;
     fprev = 0;
     xprev = zeros(512);
     t = 1;
@@ -97,34 +81,31 @@ for  i = 1 : 1%numel(filenames)
 
     %tstart=tic;
     converged = false;
-
+    
     while(~converged)
-            %converged = (abs((fval - fprev)/fval) < rel_tol) & (norm(y - Phit(xt), 2) <= epsilon);
-            %converged = converged | (norm(xt(:) - xprev(:), 2)/norm(xt(:), 2) <= rel_tol2);
-            converged = converged | (t + 1 >= max_iter);
-            fprev = fval;
-            xprev = xt;
-            xt = Psi(shrink(Psit(xt - delta*normalise(real(Phi({st + nt - vt})))), delta * rho^(-1)));
-            st = cell2mat(Phit(xt)) - y;
-            nt = sc(vt - st);
-            vt = vt - (st + nt);
-            t = t + 1;
-            fval = f(xt);
-            %fprintf("%e\n", (abs((fval - fprev)/fval) < rel_tol));
+        converged = (abs((fval - fprev)/fval) < rel_tol) & (norm(y - cell2mat(Phi_t(xt)), 2) <= epsilon);
+        converged = converged | (norm(xt(:) - xprev(:), 2)/norm(xt(:), 2) <= rel_tol2);
+        converged = converged | (t + 1 >= max_iter);
+        fprev = fval;
+        xprev = xt;
+        xt = Psi(shrink(Psi_t(xt - delta*real(Phi({st + nt - vt}))), delta * rho^(-1)));
+        %test1 = delta*real(Phi(st + nt - vt));
+        %test2 = Psit(xt - test1);
+        %test3 = shrink(test2, delta * rho^(-1));
+        %xt = Psi(test3);
+        st = cell2mat(Phi_t(xt)) - y;
+        nt = sc(vt - st);
+        vt = vt - (st + nt);
+        t = t + 1;
+        fval = f(xt);
+        %fprintf("%e\n", (abs((fval - fprev)/fval) < rel_tol));
     end
-    %gt = imread(['../datasets/augmented_dataset_linscale/test/' filenames_gt(i).name]);
+    
+    rsnr = 20*log10(norm(gt(:))/norm(gt(:)-xt(:)));
     result = cat(2,gt,bp,xt);
     imshow(result);
+    fprintf("Reconstructed and saved image to %s after %d iterations, snr: %d\n", ['../m1/' filenames(i).name], t, rsnr);
     
-    %fitswrite(xt, ['../DNNastro/ADMM_reconstructions/' filename]);
-    f = fullfile(path,'ADMM_reconstructions',filename);
-    fitswrite(xt, f);
-    snr = 20*log10(norm(gt(:))/norm(gt(:)-xt(:)));
-    fprintf(file, "%s : SNR: %e\n", filename, snr);
-    imshow(result);
-    
-    
-    cd(path);
 end
 
 function A_norm = normalise(A)
